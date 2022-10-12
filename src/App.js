adb1283c9fef8ea04f17eb2d73002f1ea0ca120b
Nagaraj Krishnappa 1016012 <nagaraj.krishnappa@jda.com>
2022-10-12 18:59:15 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import axios from "axios";
 
-import DisplayItem  from './DisplayItem';
-import Modal from './Modal';
+//import DisplayItem  from './DisplayItem';
+import DisplayItems from './DisplayItems';
+//import Modal from './Modal';
 
 class App extends React.Component {
-  state = {images: []};
+  state = {images: [], qual_imgs: []};
 
   componentDidMount() {
+    // axios.get(
+    //   "https://google-photos-album-demo2.glitch.me/4eXXxxG3rYwQVf948"
+    // ).then(response => {
+    //   this.setState({
+    //     images: response.data
+    //   });
+    // });
+
     axios.get(
-      "https://google-photos-album-demo2.glitch.me/4eXXxxG3rYwQVf948"
+      "http://127.0.0.1:8000/quality_test"
     ).then(response => {
       this.setState({
-        images: response.data
+        qual_imgs: response.data
       });
     });
   }
 
   renderImages() {
-    return this.state.images.map((image) => {
-      return <DisplayItem item = {image} />
-    });
+    //return this.state.images.map((image) => {
+    
+      return <DisplayItems items = {this.state.qual_imgs} />
+    //});
   }
 
   render() {
     return (
       <div className="ui divided items">
-        
-        {this.renderImages()}<Modal />
+        {this.renderImages()}
+        {/* <Modal /> */}
       </div>
     );
   }
--- src/DisplayItem.js
+++ src/DisplayItem.js
@@ -3,52 +3,41 @@ import React from 'react';
 import './Item.css';
 
 const displayItem = (props) => {
+    const src = "./images/images/" + props.defaultImage;
+
+    const renderIamges = () => {
+        return props.remainingImages.map((image, idx) => {            
+            let src = "./images/images/" + image.trim();
+            return (
+                <div className="column">
+                    <img src={src} style={{width:'100px', height: '100px'}} />
+                </div>
+            );
+        });
+    }
+
     return (
         <div className="item">
-        <div className="item">
-            <div className="image">
-                <img src={props.item} />
-            </div>
-            <div className="content">
-                <a className="header">Item name</a>
-                <div className="meta">
-                    <span className="cinema">Item specification</span>
+            <div className="item">
+                <div className="image">
+                    <img width="200px" height="200px" src={src} />
                 </div>
-                <div className="description">
-                    <p>Description</p>
-                </div>
-                <div className="extra">
-                    <div className="ui label">any label?</div>
-                    {/* <div className="ui label">
-                        <i className="globe icon"></i>
-                         Additional Languages
-                    </div> */}
+                <div className="content">
+                    <a className="header">Item name</a>
+                    <div className="meta">
+                        <span className="cinema">{props.quality_status}</span>
+                    </div>
+                    <div className="description">
+                        <p>{props.imageProps.item_description}</p>
+                    </div>
                 </div>
             </div>
+            <div style={{padding: '2px'}}></div>
 
-            
-        </div>
-        <div style={{padding: '2px'}}></div>
-        <div className="row">
-                <div className="column">
-                    <img src="./images/download (1).jpg" alt="Snow" style={{width:'100px', height: '100px'}} />
-                </div>
-                <div className="column">
-                    <img src="./images/download (2).jpg" alt="Forest" style={{width:'100px', height: '100px'}} />
-                </div>
-                <div className="column">
-                    <img src="./images/download (3).jpg" alt="Mountains" style={{width:'100px', height: '100px'}} />
-                </div>
-                <div className="column">
-                    <img src="./images/download (4).jpg" alt="Snow" style={{width:'100px', height: '100px'}} />
-                </div>
-                <div className="column">
-                    <img src="./images/download (5).jpg" alt="Forest" style={{width:'100px', height: '100px'}} />
-                </div>
-                <div className="column">
-                    <img src="./images/download (6).jpg" alt="Mountains" style={{width:'100px', height: '100px'}} />
-                </div>
+            <div className="row">
+                {renderIamges()}
             </div>
+            
         </div>
     )
 }
